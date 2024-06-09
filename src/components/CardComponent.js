7495cc90a63cf2e9c7ca3016961d7ede93ef96d8
samrouth <bichsamroth@gmail.com>
2024-06-09 12:00:11 -0100

    update a to link

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -11,7 +11,7 @@ export default function CardComponent() {
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            bulk of the cards content.
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
