714f09d905ad3606dd2dbe5eecb6720c36322b8d
samrouth <bichsamroth@gmail.com>
2024-06-09 11:33:53 -0100

    update

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div class="card text-center">
+        <div class="card-header">Featured</div>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+        <div class="card-footer text-body-secondary">@Copy</div>
+      </div>
+    </footer>
+  );
+}
